2c7278c56427b12c3ef5136cd26fdebbbd554ad6
Gabriel Takao <83586924+Gabrie1Tk@users.noreply.github.com>
2025-06-27 09:58:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calcularMedia() {
+function calcMedia() {
     let n1 = prompt("Digite o primeiro número:");
     let n2 = prompt("Digite o segundo número:");
     let n3 = prompt("Digite o terceiro número:");
