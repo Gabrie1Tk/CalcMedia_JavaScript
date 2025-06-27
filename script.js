b20fe48aa3889aa7f02d4c3f6b5182174952aea4
Gabriel Takao <83586924+Gabrie1Tk@users.noreply.github.com>
2025-06-27 09:14:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function calcularMedia() {
+    let n1 = prompt("Digite o primeiro número:");
+    let n2 = prompt("Digite o segundo número:");
+    let n3 = prompt("Digite o terceiro número:");
+    let n4 = prompt("Digite o quarto número:");
+
+    n1 = Number(n1);
+    n2 = Number(n2);
+    n3 = Number(n3);
+    n4 = Number(n4);
+
+    let media = (n1 + n2 + n3 + n4) / 4;
+
+    console.log(`Números usados: ${n1}, ${n2}, ${n3}, ${n4}`);
+    console.log("A média dos números é: " + media);
+}
